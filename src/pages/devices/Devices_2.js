b5fec2e97f267901e926225cb087b650cecfd6fe
teiavava <teia_vava@yahoo.com>
2021-04-16 11:21:12 +0300

    debug

--- src/pages/devices/Devices_2.js
+++ src/pages/devices/Devices_2.js
@@ -1,51 +1,45 @@
-import React, {useEffect, useState} from "react";
-import {withStyles, makeStyles, lighten} from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import {api, globalData} from "../../repo/api.js"
-import "./Devices.scss"
-import { MDBContainer } from 'mdbreact';
-import {Redirect} from "react-router";
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import Title from '../../components/Title';
-import EditDevice from "../../components/EditDevice"
-import DeleteDevice from "../../components/DeleteDevice"
-import TablePagination from "@material-ui/core/TablePagination";
-import Checkbox from "@material-ui/core/Checkbox";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
-import PropTypes from "prop-types";
-import Toolbar from "@material-ui/core/Toolbar";
-import clsx from "clsx";
-import Typography from "@material-ui/core/Typography";
-import Tooltip from "@material-ui/core/Tooltip";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import FilterListIcon from "@material-ui/icons/FilterList";
-import Switch from "@material-ui/core/Switch";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
-const StyledTableCell = withStyles((theme) => ({
-    head: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-    },
-    body: {
-        fontSize: 14,
-    },
-}))(TableCell);
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
 
-const StyledTableRow = withStyles((theme) => ({
-    root: {
-        '&:nth-of-type(odd)': {
-            backgroundColor: theme.palette.action.hover,
-        },
-    },
-}))(TableRow);
-
+const rows = [
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Donut', 452, 25.0, 51, 4.9),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Honeycomb', 408, 3.2, 87, 6.5),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Jelly Bean', 375, 0.0, 94, 0.0),
+    createData('KitKat', 518, 26.0, 65, 7.0),
+    createData('Lollipop', 392, 0.2, 98, 0.0),
+    createData('Marshmallow', 318, 0, 81, 2.0),
+    createData('Nougat', 360, 19.0, 9, 37.0),
+    createData('Oreo', 437, 18.0, 63, 4.0),
+];
 
 function descendingComparator(a, b, orderBy) {
     if (b[orderBy] < a[orderBy]) {
@@ -74,16 +68,15 @@ function stableSort(array, comparator) {
 }
 
 const headCells = [
-    { id: 'name', numeric: false, label: 'Name' },
-    { id: 'description', numeric: true, label: 'Description' },
-    { id: 'created', numeric: true, label: 'Time Created' },
-    { id: 'state', numeric: true, label: 'State' },
-    { id: 'edit', numeric: true, label: 'Edit' },
-    { id: 'delete', numeric: true, label: 'Delete' },
+    { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
+    { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
+    { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
+    { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
+    { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
 ];
 
 function EnhancedTableHead(props) {
-    const { classes, order, orderBy, onRequestSort } = props;
+    const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
     const createSortHandler = (property) => (event) => {
         onRequestSort(event, property);
     };
@@ -91,6 +84,14 @@ function EnhancedTableHead(props) {
     return (
         <TableHead>
             <TableRow>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        indeterminate={numSelected > 0 && numSelected < rowCount}
+                        checked={rowCount > 0 && numSelected === rowCount}
+                        onChange={onSelectAllClick}
+                        inputProps={{ 'aria-label': 'select all desserts' }}
+                    />
+                </TableCell>
                 {headCells.map((headCell) => (
                     <TableCell
                         key={headCell.id}
@@ -106,7 +107,7 @@ function EnhancedTableHead(props) {
                             {headCell.label}
                             {orderBy === headCell.id ? (
                                 <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? '' : ''}
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
                 </span>
                             ) : null}
                         </TableSortLabel>
@@ -188,33 +189,37 @@ EnhancedTableToolbar.propTypes = {
     numSelected: PropTypes.number.isRequired,
 };
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+    paper: {
+        width: '100%',
+        marginBottom: theme.spacing(2),
+    },
     table: {
-        paddingTop: '50',
+        minWidth: 750,
     },
-    container: {
-        paddingTop: '50',
+    visuallyHidden: {
+        border: 0,
+        clip: 'rect(0 0 0 0)',
+        height: 1,
+        margin: -1,
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        top: 20,
+        width: 1,
     },
-});
+}));
 
 export default function Devices() {
     const classes = useStyles();
-    let [devices, setDevices] = useState([]);
-    useEffect(() => {
-        async function fetch(){
-            setLoading(true)
-            let res = await api.getDevices();
-            setLoading(false)
-            setDevices(res.data.devices)
-            globalData.setTitle("Devices");
-        }
-        fetch()
-    }, [])
-
     const [order, setOrder] = React.useState('asc');
     const [orderBy, setOrderBy] = React.useState('calories');
     const [selected, setSelected] = React.useState([]);
     const [page, setPage] = React.useState(0);
+    const [dense, setDense] = React.useState(false);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
     const handleRequestSort = (event, property) => {
@@ -225,7 +230,7 @@ export default function Devices() {
 
     const handleSelectAllClick = (event) => {
         if (event.target.checked) {
-            const newSelecteds = devices.map((n) => n.name);
+            const newSelecteds = rows.map((n) => n.name);
             setSelected(newSelecteds);
             return;
         }
@@ -261,34 +266,25 @@ export default function Devices() {
         setPage(0);
     };
 
+    const handleChangeDense = (event) => {
+        setDense(event.target.checked);
+    };
+
     const isSelected = (name) => selected.indexOf(name) !== -1;
 
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, devices.length - page * rowsPerPage);
-
-
-    let [isLoading, setLoading] = useState(false);
-    let [deviceClicked, setDeviceClicked] = useState(false);
-
-    function onDeviceClick(device){
-        globalData.device = device;
-        setDeviceClicked(true)
-    }
-
-    function onDeleteClick(device){
-        setDevices(devices.filter(it => it._id != device._id))
-    }
-
-    if(deviceClicked){
-        return <Redirect to="/device"/>
-    }
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
     return (
-        <MDBContainer>
-            <p className="ml-15 ml-lg-0">
-                <Title>' '</Title>
-                <Title>Devices</Title>
-                <Table size="large" className={classes.table}>
-                    <TableHead>
+        <div className={classes.root}>
+            <Paper className={classes.paper}>
+                <EnhancedTableToolbar numSelected={selected.length} />
+                <TableContainer>
+                    <Table
+                        className={classes.table}
+                        aria-labelledby="tableTitle"
+                        size={dense ? 'small' : 'medium'}
+                        aria-label="enhanced table"
+                    >
                         <EnhancedTableHead
                             classes={classes}
                             numSelected={selected.length}
@@ -296,45 +292,63 @@ export default function Devices() {
                             orderBy={orderBy}
                             onSelectAllClick={handleSelectAllClick}
                             onRequestSort={handleRequestSort}
-                            rowCount={devices.length}
+                            rowCount={rows.length}
                         />
-                    </TableHead>
-                    <TableBody>
-                        {stableSort(devices, getComparator(order, orderBy))
-                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((it) => {
-                                return (
-                                        <StyledTableRow  key={it.id}>
-                                        <StyledTableCell onClick={() => onDeviceClick(it)}>{it.name}</StyledTableCell>
-                                        <StyledTableCell onClick={() => onDeviceClick(it)}>{it.description}</StyledTableCell>
-                                        <StyledTableCell onClick={() => onDeviceClick(it)}>{it.timestamp}</StyledTableCell>
-                                        <StyledTableCell>Connected</StyledTableCell>
-                                        <StyledTableCell>
-                                            <EditDevice device={it}></EditDevice>
-                                        </StyledTableCell>
-                                        <StyledTableCell onClick={() => onDeleteClick(it)}>
-                                            <DeleteDevice device={it}></DeleteDevice>
-                                        </StyledTableCell>
-                                    </StyledTableRow>)}
-                                )
-                            }
-                        {emptyRows > 0 && (
-                            <TableRow>
-                                <TableCell colSpan={6} />
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
+                        <TableBody>
+                            {stableSort(rows, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row, index) => {
+                                    const isItemSelected = isSelected(row.name);
+                                    const labelId = `enhanced-table-checkbox-${index}`;
+
+                                    return (
+                                        <TableRow
+                                            hover
+                                            onClick={(event) => handleClick(event, row.name)}
+                                            role="checkbox"
+                                            aria-checked={isItemSelected}
+                                            tabIndex={-1}
+                                            key={row.name}
+                                            selected={isItemSelected}
+                                        >
+                                            <TableCell padding="checkbox">
+                                                <Checkbox
+                                                    checked={isItemSelected}
+                                                    inputProps={{ 'aria-labelledby': labelId }}
+                                                />
+                                            </TableCell>
+                                            <TableCell component="th" id={labelId} scope="row" padding="none">
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell align="right">{row.calories}</TableCell>
+                                            <TableCell align="right">{row.fat}</TableCell>
+                                            <TableCell align="right">{row.carbs}</TableCell>
+                                            <TableCell align="right">{row.protein}</TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
                 <TablePagination
                     rowsPerPageOptions={[5, 10, 25]}
                     component="div"
-                    count={devices.length}
+                    count={rows.length}
                     rowsPerPage={rowsPerPage}
                     page={page}
                     onChangePage={handleChangePage}
                     onChangeRowsPerPage={handleChangeRowsPerPage}
                 />
-            </p>
-        </MDBContainer>
+            </Paper>
+            <FormControlLabel
+                control={<Switch checked={dense} onChange={handleChangeDense} />}
+                label="Dense padding"
+            />
+        </div>
     );
 }
