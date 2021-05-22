96164e23c43e8baaef124d606b3de44108a29082
teiavava <teia_vava@yahoo.com>
2021-05-22 15:42:06 +0300

    users per day works

--- src/pages/admin/GoogleAnalytics.js
+++ src/pages/admin/GoogleAnalytics.js
@@ -1,53 +1,18 @@
-// import React from "react";
-// import { AnalyticsDashboard } from "react-analytics-charts";
-// import { SessionsByDateChart, SessionsGeoChart, SessionsBySourceChart, PageViewsPerPathChart , SessionsByHourChart} from "react-analytics-charts";
-//
-// export default function GoogleAnalytics() {
-//     return (
-//         <AnalyticsDashboard style={{color: "white"}}
-//             authOptions={{ clientId: "306633733491-q51dbb4a7m882su9dfpqsdmna2lq7pcq.apps.googleusercontent.com" }}
-//             renderCharts={(gapi, viewId) => {
-//                 const chartStyles = {
-//                     margin: "15px",
-//                     maxWidth: 400,
-//                 };
-//                 return (
-//                     <div style={{ display: "flex", flexWrap: "wrap" }}>
-//                         <SessionsByDateChart
-//                             gapi={gapi}
-//                             viewId={viewId}
-//                             style={chartStyles}
-//                             showPageViews
-//                             showUsers
-//                         />
-//                         <SessionsGeoChart
-//                             gapi={gapi}
-//                             viewId={viewId}
-//                             style={chartStyles}
-//                             showPageViews
-//                             options={{ width: 400 }}
-//                         />
-//                         <SessionsBySourceChart
-//                             gapi={gapi}
-//                             viewId={viewId}
-//                             style={chartStyles}
-//                         />
-//                         <SessionsByHourChart gapi={gapi} viewId={viewId} style={chartStyles} />
-//                         <PageViewsPerPathChart
-//                             gapi={gapi}
-//                             viewId={viewId}
-//                             style={{ margin: "15px" }}
-//                         />
-//                     </div>
-//                 );
-//             }}
-//         />
-//     );
-// }
-
 import React, { useState, useEffect } from "react";
 import { renderButton, checkSignedIn } from "../../utils";
 import Report from "../../report";
+import {addDays} from "date-fns";
+import { Line } from "react-chartjs-2";
+import {
+    ChartWrapper,
+    ReportWrapper,
+    ChartTitle,
+    Subtitle,
+    DatepickerRow,
+} from "./styles";
+import CustomDatePicker from "./datepicker";
+import { queryReport } from "./queryReport";
+import { formatDate } from "./utils";
 
 function GoogleAnalytics() {
     const [isSignedIn, setIsSignedIn] = useState(false);
@@ -73,6 +38,97 @@ function GoogleAnalytics() {
         window.gapi.load("auth2", init); //(1)
     });
 
+    const INITIAL_STATE = {
+        labels: [],
+        values: [],
+    };
+    const [reportData, setReportData] = useState(INITIAL_STATE);
+    const [startDate, setStartDate] = useState(addDays(new Date(), -10));
+    const [endDate, setEndDate] = useState(new Date());
+    const [average, setAverage] = useState(0);
+
+    const displayResults = (response) => {
+        const queryResult = response.result.reports[0].data.rows;
+        const total = response.result.reports[0].data.totals[0].values[0];
+        setAverage(parseInt(total / response.result.reports[0].data.rowCount));
+        let labels = [];
+        let values = [];
+        queryResult.forEach((row) => {
+            labels.push(formatDate(row.dimensions[0]));
+            values.push(row.metrics[0].values[0]);
+        });
+        setReportData({
+            ...reportData,
+            labels,
+            values,
+        });
+    };
+
+    const data = {
+        labels: reportData.labels,
+        datasets: [
+            {
+                label: `Users per day`,
+                fill: false,
+                lineTension: 0.3,
+                borderColor: "#35213d",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "#375751",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: reportData.values,
+            },
+        ],
+    };
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        suggestedMin: 0,
+                    },
+                },
+            ],
+            xAxes: [
+                {
+                    ticks: {
+                        autoSkip: true,
+                        maxTicksLimit: 7,
+                    },
+                },
+            ],
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        plugins: {
+            datalabels: {
+                font: {
+                    size: 0,
+                },
+            },
+        },
+    };
+
+    useEffect(() => {
+        const request = {
+            viewID: "243443456",
+            startDate,
+            endDate,
+            metrics: "ga:sessions",
+            dimensions: ["ga:date"],
+        };
+        queryReport(request)
+            .then((resp) => displayResults(resp))
+            .catch((error) => console.error(error));
+    }, [startDate, endDate]);
+
+
     return (
         <div>
             {!isSignedIn ? (
@@ -80,6 +136,27 @@ function GoogleAnalytics() {
             ) : (
                 <div style={{backgroundColor: "white"}}>
                     <Report />
+                    <ReportWrapper>
+                        <ChartTitle>{`Users per day`}</ChartTitle>
+                        <Subtitle>{`Average - ${average} Users`}</Subtitle>
+                        <DatepickerRow>
+                            <CustomDatePicker
+                                placeholder={"Start date"}
+                                date={startDate}
+                                handleDateChange={(date) => setStartDate(date)}
+                            />
+                            <CustomDatePicker
+                                placeholder={"End date"}
+                                date={endDate}
+                                handleDateChange={(date) => setEndDate(date)}
+                            />
+                        </DatepickerRow>
+                        {reportData && (
+                            <ChartWrapper>
+                                <Line data={data} options={options} width={100} height={250} />
+                            </ChartWrapper>
+                        )}
+                    </ReportWrapper>
                 </div>
             )}
         </div>
