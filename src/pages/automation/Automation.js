fd9570c7a1de149cb06836a9566cfaadec8ad9f7
teiavava <teia_vava@yahoo.com>
2021-06-15 21:34:37 +0300

    fix nodered

--- src/pages/automation/Automation.js
+++ src/pages/automation/Automation.js
@@ -2,7 +2,7 @@ import React from "react";
 import Iframe from 'react-iframe'
 
 export default function Automation() {
-    return <Iframe url="http://localhost:1880"
+    return <Iframe url="http://localhost:1880/?#flow/1a8d7be3.7989c4"
                    width="97%"
                    height="750px"
                    id="nodered"
